bb9a97c0ca69879af8662cfc9f038e4481219425
bonnerac <alex.c.bonner@gmail.com>
2020-06-06 20:44:15 -0500

    added contact section to bottom of sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FiFileText } from "react-icons/fi";
+import { MdEmail } from "react-icons/md";
 import '../sidebar/style.css'
 
 
@@ -19,6 +23,27 @@ function Sidebar() {
                     <div>
                         <a href='#about'> About </a>
                     </div>
+                </div>
+                <div className="contact">
+                    <div className="contactTitle">
+                        Contact
+                    </div>
+                    <div className="contactLinks">
+                        <a href="https://github.com/bonnerac">
+                            <FaGithub />
+                        </a>
+                        <a href="https://www.linkedin.com/in/alexbonner/">
+                            <FaLinkedin />
+                        </a>
+                        <a href="https://docs.google.com/document/d/1eGtjoGfXdGk00iiEWivzXKpOt-24r7HaGVv7rRbK6Mk/edit?usp=sharing">
+                            <FiFileText />
+                        </a>
+                        <a href="mailto:alex.c.bonner@gmail.com">
+                            <MdEmail />
+                        </a>
+
+                    </div>
+
                 </div>
 
             </div>
